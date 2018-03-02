b510b9e87cf82114977c04be03477faa75479f7b
Prog24 <prog24.net@gmail.com>
2018-03-02 22:26:20 +0900

    Fix search-shelf-book-data

--- public/api2/search-shelf-book-data.php
+++ public/api2/search-shelf-book-data.php
@@ -1,12 +1,23 @@
 <?php
 
-require_once(__DIR__.'/../../src/connect.php');
-require_once(__DIR__.'/../../src/Books.php');
+function searchShelfBookData()
+{
+  require_once(__DIR__.'/checkLoginAPI.php');
+  $check = checkLogin();
+  if($check['status'] == '400')
+  {
+    exit('400');
+  }else{
+    require_once(__DIR__.'/../../src/Books.php');
+    $pdo = $check['pdo'];
+    $books = new Books($pdo, 'books');
+    
+    $id = 3;
+    
+    $book_data = $books->searchShelfBookData($id);
+    return $book_data;
+  }
+}
 
-$pdo = connect();
-$books = new Books($pdo, 'books');
-
-$id = 3;
-
-$book_data = $books->searchShelfBookData($id);
-var_dump($book_data);
+$books = searchShelfBookData();
+var_dump($books);
