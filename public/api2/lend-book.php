f85091e7bad8e0bcb896f113cbe1d5c62e3d1af9
Prog24 <prog24.net@gmail.com>
2018-03-02 22:15:05 +0900

    Fix lend-book

--- public/api2/lend-book.php
+++ public/api2/lend-book.php
@@ -1,12 +1,26 @@
 <?php
 session_start();
-require_once(__DIR__.'/../../src/connect.php');
-require_once(__DIR__.'/../../src/Books.php');
 
-$pdo = connect();
-$books = new Books($pdo, 'books');
+function lendBook()
+{
+  require_once(__DIR__.'/checkLoginAPI.php');
+  $check = checkLogin();
+  if($check['status'] == '400')
+  {
+    exit("400");
+  }else{
+    require_once(__DIR__.'/../../src/Books.php');
+    $pdo = $check['pdo'];
+    $books = new Books($pdo, 'books');
 
-$id = 3;
-$userId = $_SESSION['userId'];
+    $id = 3;
+    $userId = $_SESSION['userId'];
 
-$books->lendBook($id, $userId);
+    $books->lendBook($id, $userId);
+    $return = array('status'=>'200');
+    return $return;
+  }
+}
+
+$return = lendBook();
+var_dump($return);
