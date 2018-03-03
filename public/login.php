6f9051e6269a87e585be57f9b1b95fd815f4a8e6
Prog24 <prog24.net@gmail.com>
2018-03-03 20:36:29 +0900

    Fix LoginPage

--- public/login.php
+++ public/login.php
@@ -1,1 +1,25 @@
-<a href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1559472553&redirect_uri=https://dev.prog24.com/public/api2/login.php&state=12345abcde&scope=openid%20profile&nonce=09876xyz">Login</a>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>BookHub</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    crossorigin="anonymous">
+	</head>
+    <body>
+      <header>
+        <div class="logo">BookHub</div>
+      </header>
+      <div class="main">
+        <div class="content">
+        <a href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1559472553&redirect_uri=https://dev.prog24.com/public/api2/login.php&state=12345abcde&scope=openid%20profile&nonce=09876xyz">Login</a>
+          
+        </div>
+      </div>
+    </body>
+</html>
