2b3281b3ffba8bae5aa988ab1e4b19d44dc1acc6
Prog24 <prog24.net@gmail.com>
2018-03-03 11:42:48 +0900

    Create Get Shelf Cate

--- src/Categories.php
+++ src/Categories.php
@@ -21,11 +21,31 @@ class Categories
     $this->dbtable = $dbtable;
   }
 
+  /**
+   * New Category
+   * 
+   * @param string $name
+   * @param int $shelf_id
+   */
   public function newCate($name, $shelf_id)
   {
     $stmt = $this->connect->prepare('INSERT INTO categories (name, shelf_id) VALUES (:name, :shelf_id)');
     $stmt->bindValue(':name', $name, PDO::PARAM_STR);
     $stmt->bindValue(':shelf_id', $shelf_id, PDO::PARAM_INT);
     $stmt->execute();
+  }
+
+  /**
+   * Get Shelf's Categories
+   * 
+   * @param int $shelf_id
+   */
+  public function getShelfCate($shelf_id)
+  {
+    $stmt = $this->connect->prepare('SELECT * FROM categories WHERE shelf_id = :shelf_id');
+    $stmt->bindParam(':shelf_id', $shelf_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $shelf = $stmt->fetchAll();
+    return $shelf;
   }
 }
