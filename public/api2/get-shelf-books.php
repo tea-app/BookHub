b6e91ed7405e6ec5987f613b1aeab4e849ba3817
Prog24 <prog24.net@gmail.com>
2018-03-03 20:22:55 +0900

    Fix API2/GET-

--- public/api2/get-shelf-books.php
+++ public/api2/get-shelf-books.php
@@ -1,22 +1,49 @@
 <?php
 /**
  * @param PDO
+ * @param int $id
+ * 
+ * @return array books
  */
-function getShelfBooks($pdo)
+
+function getShelfBooks($pdo, $id)
 {
-  require_once(__DIR__.'/checkLoginAPI.php');
-  $check = checkLogin($pdo);
-  if($check['status'] == '400')
+  if($pdo)
   {
-    exit("400");
-  }else{
     require_once(__DIR__.'/../../src/Books.php');
-    $pdo = $check['pdo'];
     $books = new Books($pdo, 'books');
-    
-    $shelf_id = 3;
-    
-    $books_data = $books->searchShelfBooks($shelf_id);
-    return $books_data;
+
+
+    $books_data = $books->searchShelfBooks($id);
+    $return = array(
+      'status'=>'200',
+      'data'=>$books_data
+    );
+    return $return;
+  }else{
+    $return = array(
+      'status'=>'400'
+    );
+    return $return;
   }
+  
 }
+
+// function getShelfBooks($pdo, $id)
+// {
+//   require_once(__DIR__.'/checkLoginAPI.php');
+//   $check = checkLogin($pdo);
+//   if($check['status'] == '400')
+//   {
+//     exit("400");
+//   }else{
+//     require_once(__DIR__.'/../../src/Books.php');
+//     $pdo = $check['pdo'];
+//     $books = new Books($pdo, 'books');
+    
+//     $shelf_id = $id;
+    
+//     $books_data = $books->searchShelfBooks($shelf_id);
+//     return $books_data;
+//   }
+// }
--- public/api2/get-shelf-data.php
+++ public/api2/get-shelf-data.php
@@ -1,23 +1,48 @@
 <?php
 
-function getShelfData()
+/**
+ * @param PDO
+ * @param int $id
+ * 
+ * @return array shelf_data
+ */
+function getShelfData($pdo, $id)
 {
-  require_once(__DIR__.'/checkLoginAPI.php');
-  $check = checkLogin();
-  if($check['status'] == '200')
+  if($pdo)
   {
     require_once(__DIR__.'/../../src/Shelf.php');
-    $pdo = $check['pdo'];
     $shelfs = new Shelf($pdo, 'shelf');
 
-    $id = 1;
-
     $shelf_data = $shelfs->getShelfData($id);
-    return $shelf_data;
+    $return = array(
+      'status'=>'200',
+      'data'=>$shelf_data
+    );
+    return $return;
   }else{
-    exit('400');
+    $return = array('status'=>'400');
+    return $return;
   }
 }
 
-$shelf = getShelfData();
-var_dump($shelf);
+// function getShelfData()
+// {
+//   require_once(__DIR__.'/checkLoginAPI.php');
+//   $check = checkLogin();
+//   if($check['status'] == '200')
+//   {
+//     require_once(__DIR__.'/../../src/Shelf.php');
+//     $pdo = $check['pdo'];
+//     $shelfs = new Shelf($pdo, 'shelf');
+
+//     $id = 1;
+
+//     $shelf_data = $shelfs->getShelfData($id);
+//     return $shelf_data;
+//   }else{
+//     exit('400');
+//   }
+// }
+
+// $shelf = getShelfData();
+// var_dump($shelf);
