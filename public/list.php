54fe5d56ac4f2d30d18b1bc3ca60242942bae037
Prog24 <prog24.net@gmail.com>
2018-03-04 19:02:40 +0900

    Fix

--- public/list.php
+++ public/list.php
@@ -32,10 +32,12 @@ if($checkLogin['status'] == '200')
     <link rel="stylesheet" href="css/shelf.css">
     <link rel="stylesheet" href="css/search.css">
     <link rel="stylesheet" href="css/list.css">
+    <link rel="stylesheet" href="css/modal.css">
     <link rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
     crossorigin="anonymous">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="js/modal.js"></script>
 	</head>
   <body>
@@ -65,7 +67,7 @@ if($checkLogin['status'] == '200')
         </div>
         <div class="shelf-list">
           <?php foreach($books as $book) : ?>
-          <div class="book-content" id="<?php echo $book['id']; ?>" onClick="reaction(this);">
+          <div class="book-content" data-target="<?php echo $book['id']; ?>">
             <img src="<?php echo $book['image_url']; ?>" class="book-img"></img>
             <div class="book-info">
               <div class="book-title">
@@ -78,5 +80,20 @@ if($checkLogin['status'] == '200')
         </div>
       </div>
     </div>
+
+    <!-- ここからモーダル -->
+    <?php foreach($books as $book) : ?>
+      <div id="<?php echo $book['id']; ?>" class="modal-contents">
+        <img src="<?php echo $book['image_url']; ?>" class="modal-book-img"></img>
+        <div class="modal-info">
+          <div class="modal-title"><?php echo $book['title']; ?></div>
+          <div class="modal-writer"><?php echo $book['author']; ?></div>
+          <div class="modal-isbn">isbn:<?php echo $book['isbn']; ?></div>
+          <div class="modal-category"><?php echo $book['cate_id']; ?></div>
+        </div>
+        <button type="button" role="button" class="modal-borrow-button btn btn-lg btn-primary">借りる!</button>
+        <p><a id="modal-close" class="button-link">×</a></p>
+      </div>
+    <?php endforeach; ?>
   </body>
 </html>
